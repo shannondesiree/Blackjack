6131b2675214cc08a0c5a99d3a29b597730f00c9
shannondesiree <111889962+shannondesiree@users.noreply.github.com>
2023-01-11 11:42:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -316,13 +316,13 @@ function addCardToHand(arr) {
             document.getElementById("declare-winner").innerHTML = "You have 21. You Win!";
         } else if (dTotal === 21) {
             document.getElementById("declare-winner").innerHTML = "The dealer has 21. Dealer Wins!";
-        } else if (pTotal > dTotal && pTotal < 21) {
+        } else if (pTotal > dTotal && pTotal < 22) {
             document.getElementById("declare-winner").innerHTML = "You have: " + pTotal + ". The dealer has: " + dTotal + ". You win!";
         } else if (pTotal > 21) {
             document.getElementById("declare-winner").innerHTML = "You have gone over 21 with " + pTotal + ". Dealer wins!";
-        } else if (dTotal > pTotal && dTotal < 21) {
+        } else if (dTotal > pTotal && dTotal < 22) {
             document.getElementById("declare-winner").innerHTML = "You have: " + pTotal + " .The dealer has: " + dTotal + ". Dealer wins!";
-        } else if (pTotal < dTotal && pTotal < 21) {
+        } else if (pTotal < dTotal && pTotal < 22) {
             document.getElementById("declare-winner").innerHTML = "You have: " + pTotal + " .The dealer has: " + dTotal + ". You win!";
         } else if (pTotal === dTotal) {
             document.getElementById("declare-winner").innerHTML = "It's a Draw!"
